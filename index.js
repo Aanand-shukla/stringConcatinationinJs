a67f06074b41c045202ab1493db99cee1ac72948
Aanand-shukla <93906271+Aanand-shukla@users.noreply.github.com>
2022-08-23 17:29:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// conversion string to number and number to string.
+
+let age = "22";
+let name ="Aanand Shukla";
+let marks1 = 22;
+let marks2= 32;
+let fullMarks = marks1+"" + marks2+"";
+let details= "My name is "+ name + " My age is "+ +age +" my marks is " +fullMarks;
+
+console.log(details);
